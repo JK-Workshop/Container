570b21308748fa48140a224347441020187e0a8f
Sparc0926 <sparcjarry@gmail.com>
2025-09-29 11:41:50 -0600

    Finish union find set

--- Test/UnionFindSet.h
+++ Test/UnionFindSet.h
@@ -13,17 +13,10 @@
 #include <filesystem>
 #include <print>
 #include <string>
+#include <vector>
 
 static constexpr auto TESTCASE_COUNT = size_t(18);
 static const auto DIR = std::filesystem::path("Test/UnionFindSetCases");
-
-size_t n, m, fa[10000000];
-
-size_t find(size_t i)
-{
-   if (fa[i] == i) return i;
-   return fa[i] = find(fa[i]);
-}
 
 auto Test_UnionFindSet() noexcept -> void
 {
@@ -37,11 +30,17 @@ auto Test_UnionFindSet() noexcept -> void
       // Read in data count and operation count
       size_t data_c, op_c;
       fscanf(inHandle, "%zu%zu", &data_c, &op_c);
-      for (auto i = size_t(0); i < data_c; ++i) {
-         fa[i] = i;
-      }
+      // Define the data list
+      auto data_l = std::make_unique<size_t[]>(data_c);
+      for (auto i = size_t(0); i < data_c; ++i)
+         data_l[i] = i;
+
       struct inCache_t {
          size_t op_v, a, b;
+      };
+      struct inCacheP_t {
+         size_t op_v;
+         size_t *p, *q;
       };
       // Cache into list from input file, avoiding future reading file stall
       auto inCache_l = std::make_unique<inCache_t[]>(op_c);
@@ -53,33 +52,21 @@ auto Test_UnionFindSet() noexcept -> void
       while (fscanf(outHandle, "%zu", &outCache_l[outCache_i]) != EOF)
          ++outCache_i;
       outCache_i = 0;
-      // Define the data list
-      auto data_l = std::make_unique<size_t[]>(data_c);
-      for (auto i = size_t(0); i < data_c; ++i)
-         data_l[i] = i;
 
       auto iUfs = JK::IdxUnionFindSet<std::dynamic_extent>(data_c);
-      auto pUfs = JK::PtrUnionFindSet<size_t, std::dynamic_extent>(data_c);
 
       // Test index union find set
-      {
-         size_t cnt = 0;
-         const auto timerName = std::format("Index union find set testcase {} passed", fileIdx);
-         auto timer = ScopedTimer(timerName);
-         for (auto i = size_t(0); i < op_c; ++i) {
-            const auto [op_v, a, b] = inCache_l[i];
-            if (op_v == 0) fa[find(a)] = find(b);
-            else if (outCache_l[outCache_i++] != (find(a) == find(b))) {
-               std::print("Testcase {} failed\n", fileIdx);
-               __debugbreak();
-            }
+      const auto timerName = std::format("Index union find set testcase {} passed", fileIdx);
+      auto timer = ScopedTimer(timerName);
+      for (auto i = size_t(0); i < op_c; ++i) {
+         const auto [op_v, a, b] = inCache_l[i];
+         if (op_v == 0) iUfs.Unite(a, b);
+         else if (outCache_l[outCache_i++] != iUfs.IsSame(a, b)) {
+            std::print("Testcase {} failed\n", fileIdx);
+            JK_RUNTIME_BREAK();
          }
-         std::print("{}\n", cnt);
       }
-      // Test pointer union find set
-      {
-         // auto timer = ScopedTimer(std::format("Pointer union find set testcase {} passed", fileIdx));
-      }
+      timer.~ScopedTimer();
 
       fclose(inHandle);
       fclose(outHandle);
--- include/UnionFindSet.h
+++ include/UnionFindSet.h
@@ -40,9 +40,11 @@ namespace JK {
       {
       }
 
-      constexpr auto Unite(SIZE_T p_Target0_i, SIZE_T p_Target1_i) noexcept -> void;
+      constexpr auto Unite(const SIZE_T p_Target0_i, const SIZE_T p_Target1_i) noexcept -> void;
 
-      JK_NODISCARD constexpr auto Find(SIZE_T p_Target_i) noexcept -> SIZE_T;
+      JK_NODISCARD constexpr auto Find(const SIZE_T p_Target_i) noexcept -> const SIZE_T;
+
+      JK_NODISCARD constexpr auto IsSame(const SIZE_T p_Target0_i, const SIZE_T p_Target1_i) noexcept -> const bool { return this->Find(p_Target0_i) == this->Find(p_Target1_i); }
 
     private:
       TUPLE_LIST_T tuple_l = TUPLE_LIST_T{};
@@ -50,104 +52,38 @@ namespace JK {
       SIZE_T cnt = SIZE_T(0); // Stack counter
    };
 
-   TEMPLATE constexpr auto CLASS_NAME::Unite(SIZE_T p_Target0_i, SIZE_T p_Target1_i) noexcept -> void
+   TEMPLATE constexpr auto CLASS_NAME::Unite(const SIZE_T p_Target0_i, const SIZE_T p_Target1_i) noexcept -> void
    {
       const auto i = this->Find(p_Target0_i);
       const auto j = this->Find(p_Target1_i);
       if (i == j) return;
-      if (this->treeSize_l[i] < this->treeSize_l[j]) {
-         this->parentIdx_l[i] = j;
-         this->treeSize_l[j] += this->treeSize_l[i];
+      if (this->tuple_l[i].Tree_s < this->tuple_l[j].Tree_s) {
+         this->tuple_l[i].Parent_i = j;
+         this->tuple_l[j].Tree_s += this->tuple_l[i].Tree_s;
       } else {
-         this->parentIdx_l[j] = i;
-         this->treeSize_l[i] += this->treeSize_l[j];
+         this->tuple_l[j].Parent_i = i;
+         this->tuple_l[i].Tree_s += this->tuple_l[j].Tree_s;
       }
    }
 
-   TEMPLATE constexpr auto CLASS_NAME::Find(SIZE_T p_Target_i) noexcept -> SIZE_T
+   TEMPLATE constexpr auto CLASS_NAME::Find(const SIZE_T p_Target_i) noexcept -> const SIZE_T
    {
       auto i = this->tuple_l[p_Target_i].Parent_i;
-      if (i == -1)  return p_Target_i;
-      while (1) {
+      if (i == -1) return p_Target_i;
+      while (true) {
          const auto j = this->tuple_l[i].Parent_i;
          if (j == -1) {
-            do this->tuple_l[this->stack[--this->cnt]].Parent_i = i;
-            while (this->cnt);
+            while (this->cnt != 0)
+               this->tuple_l[this->stack[--this->cnt]].Parent_i = i;
             return i;
          }
-         this->stack[cnt++] = i;
+         this->stack[this->cnt++] = i;
          i = j;
       }
    }
 
 #undef TEMPLATE
 #undef CLASS_NAME
-
-#define TEMPLATE template <class DATA_T, std::unsigned_integral auto SET_S>
-#define CLASS_NAME PtrUnionFindSet<DATA_T, SET_S>
-
-   TEMPLATE class PtrUnionFindSet {
-    private:
-      using SIZE_T = decltype(SET_S);
-      struct pair {
-         DATA_T* Data_p;
-         pair* Parent_p = pair * (nullptr);
-         SIZE_T Tree_s = SIZE_T(1);
-      };
-      using PAIR_LIST_T = std::conditional_t<SET_S == -1, std::unique_ptr<pair[]>, std::array<pair, SET_S>>;
-      using LIST_T = std::conditional_t<SET_S == -1, std::unique_ptr<SIZE_T[]>, std::array<SIZE_T, SET_S>>;
-
-    public:
-      using SIZE_TYPE = SIZE_T;
-      static constexpr auto SET_SIZE = SET_S;
-
-    public:
-      constexpr PtrUnionFindSet() noexcept
-         requires(SET_S != -1)
-      {
-      }
-
-      constexpr PtrUnionFindSet(const SIZE_T p_Set_s) noexcept
-         requires(SET_S == -1)
-         : pair_l(std::make_unique<pair[]>(p_Set_s))
-      {
-      }
-
-      constexpr auto Unite(const DATA_T* p_DataPtr0_v, const DATA_T* p_DataPtr1_v) noexcept -> void;
-
-      constexpr auto Find(const DATA_T* p_DataPtr_v) noexcept -> DATA_T*;
-
-      constexpr auto Same(const DATA_T* p_DataPtr0_v, const DATA_T* p_DataPtr1_v) noexcept -> bool { return this->Find(p_DataPtr0_v) == this->Find(p_DataPtr1_v); }
-
-    private:
-      PAIR_LIST_T pair_l;
-   };
-
-   TEMPLATE constexpr auto CLASS_NAME::Unite(const DATA_T* p_DataPtr0_v, const DATA_T* p_DataPtr1_v) noexcept -> void
-   {
-      const auto p = static_cast<pair*>(this->Find(p_DataPtr0_v));
-      const auto q = static_cast<pair*>(this->Find(p_DataPtr1_v));
-      if (p == q) return;
-      if (p->Tree_s < q->Tree_s) {
-         p->Parent_p = static_cast<DATA_T*>(q);
-         q->Tree_s += p->Tree_s;
-      } else {
-         q->Parent_p = static_cast<DATA_T*>(p);
-         p->Tree_s += q->Tree_s;
-      }
-   }
-
-   TEMPLATE constexpr auto CLASS_NAME::Find(const DATA_T* p_DataPtr_v) noexcept -> DATA_T*
-   {
-      auto p = static_cast<pair*>(p_DataPtr_v)->Parent_p;
-      while (p != nullptr) {
-         auto gp = p->Parent_p;
-         if (gp == nullptr)
-            return p;
-         p = gp;
-      }
-      return p_DataPtr_v;
-   }
 
 } // namespace JK
 
